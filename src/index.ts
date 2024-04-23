0f03ffae2e55c72ec3f9504af043ec44fcbe2e9e
ljaviertovar <luisjavier_tovar@outlook.com>
2024-04-23 00:40:07 -0400

    add connections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('yeah')
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import { db } from './database'
+
+dotenv.config()
+
+const app = express()
+
+app.use(
+	cors({
+		credentials: true,
+	})
+)
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(3000, () => {
+	console.log('Server is running on port 3000')
+})
+
+try {
+	db.connect()
+} catch (error) {
+	console.error(error)
+}
