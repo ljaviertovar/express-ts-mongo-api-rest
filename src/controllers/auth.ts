5f3599a5e918f5a48a2795750707d0750d794394
Javier <luis.tovar@corebiz.ag>
2024-05-12 23:39:04 -0400

    add register and login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -33,3 +33,39 @@ export const register = async (req: express.Request, res: express.Response) => {
 		return res.status(500).json({ error: error.message })
 	}
 }
+
+export const login = async (req: express.Request, res: express.Response) => {
+	try {
+		const { email, password } = req.body
+
+		if (!email || !password) {
+			return res.status(400).json({ error: 'Missing required fields' })
+		}
+
+		const user = await getUserByEmail(email).select('+authentication.password +authentication.salt')
+		if (!user) {
+			return res.status(400).json({ error: 'User not found' })
+		}
+
+		if (user.authentication.password !== authentication(user.authentication.salt, password)) {
+			return res.status(403).json({ error: 'Incorrect password' })
+		}
+
+		// Generate a new session token
+		const salt = random()
+		user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+		await user.save()
+
+		// Set the session token as a cookie
+		res.cookie('AUTH_TOKEN', user.authentication.sessionToken, {
+			domain: 'localhost',
+			path: '/',
+		})
+
+		return res.status(200).json(user).end()
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ error: error.message })
+	}
+}
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 
-import { register } from '../controllers/auth'
+import { register, login } from '../controllers/auth'
 
 export default (router: express.Router) => {
 	router.post('/auth/register', register)
+	router.post('/auth/login', login)
 }
